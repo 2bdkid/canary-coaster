24d6e9a6b6fd3648cabb63edc63bbe332451b99d
Brady Dean <2bdkid@gmail.com>
2020-10-15 14:05:54 -0400

    added get_well-known_core.py

---
+++ get_well-known_core.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import asyncio
+import json
+
+from aiocoap import Context
+from aiocoap import Message
+from aiocoap import GET
+from aiocoap.util.linkformat import parse
+
+
+async def main():
+    protocol = await Context.create_client_context()
+    get_rd_req = Message(code=GET, uri='coap://192.168.1.254/.well-known/core')
+    response = await protocol.request(get_rd_req).response
+    linkformat = parse(response.payload.decode('ascii'))
+    rd_data = json.loads(linkformat.as_json_string())
+    
+    for resource in rd_data:
+        print('%s%s' % (response.remote.uri_base, resource['href']))
+    
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
