925bd2cc09f42ceb82c89ce36693a8455abca750
Jake Waggoner <waggonerjake99@gmail.com>
2020-11-18 12:18:38 -0500

    Added some images to the UI

--- UI/index.php
+++ UI/index.php
@@ -26,23 +26,50 @@
 			document.getElementById("tareButton").innerHTML = "";
 			for(const [key, value] of Object.entries(weights))
 			{
+				var weight = parseFloat(value).toFixed(2);
+				var image = document.getElementById("cup");
+				var pathToImage = "images/";
+				if(weight > 670)
+				{
+					image.src = pathToImage+"FullGlass.PNG";
+					image.style.height = "180px";
+					image.style.width = "123px";
+				}
+				else if(weight > 600 && weight <= 670)
+				{
+					image.src = pathToImage+"MostlyFullGlass.PNG";
+					image.style.height = "180px";
+					image.style.width = "123px";
+				}
+				else if(weight > 530 && weight <= 600)
+				{
+					image.src = pathToImage+"HalfWayFullGlass.PNG";
+					image.style.height = "180px";
+					image.style.width = "123px";
+				}
+				else if(weight > 430 && weight <= 530)
+				{
+					image.src = pathToImage+"MostlyEmptyGlass.PNG";
+					image.style.height = "180px";
+					image.style.width = "123px";
+				}
+				else if(weight > 320 && weight <= 430)
+				{
+					image.src = pathToImage+"EmptyGlass.PNG";
+					image.style.height = "180px";
+					image.style.width = "123px";
+				}
+				else
+				{
+					image.src = pathToImage+"X.png";
+					image.style.height = "110px";
+					image.style.width = "110px";
+				}
 				document.getElementById("data").innerHTML += key + ": " + parseFloat(value).toFixed(2);
-				document.getElementById("tareButton").innerHTML += '<button onclick="webSocket.send(`'+key+'`); console.log(`'+key+'`)">Tare '+key+'</button>';
+				document.getElementById("tareButton").innerHTML += '<button onclick="webSocket.send(`'+key+'`)">Tare '+key+'</button>';
 			}
 		}
 	</script>
-		<!-- Header -->
-			<header id="header" class="alt">
-				<nav id="nav">
-					<ul>
-						<li><a href="index.php">Home</a></li>
-						<li><a href="index.php">Generic</a></li>
-						<li><a href="index.php">Elements</a></li>
-					</ul>
-				</nav>
-			</header>
-
-			<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
 
 		<!-- Banner -->
 			<section id="banner">
@@ -53,14 +80,24 @@
 			<section id="one" class="wrapper style1">
 				<div class="container">
 					<header class="major">
-						<h2 id="data" name="data">Data</h2>
+						<img id="cup" src="images/FullGlass.PNG"><br>
+						<p id="data" name="data">Data</p>
 						<div id="tareButton" name = "tareButton"></div>
 					</header>
 				</div>
 			</section>
 			
-		<!-- Divider -->
-		<section id="four" class="wrapper style3 special"></section>
+		<!-- Footer -->
+		<footer id="footer">
+			<div class="container">
+				<ul class="icons">
+					<li><a href="https://github.iu.edu/bddean/iot-project3">GitHub</a></li>
+				</ul>
+				<ul class="copyright">
+					<li>Created By Brady Dean and Jake Waggoner</li>
+				</ul>
+			</div>
+		</footer>
 
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
